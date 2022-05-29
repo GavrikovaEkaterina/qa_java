1a02759effae6956ebea3549e2f73898454b6a9d
Ekaterina Gavrikova <gavrikova19.1997@yandex.ru>
2022-05-29 21:50:27 +0400

    Save all changes

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -2,33 +2,32 @@ package com.example;
 
 import java.util.List;
 
-public class Lion extends Animal implements Predator {
+public class Lion {
 
     boolean hasMane;
+    private Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
+    }
+
+
+    public int getKittens() {
+        return feline.getKittens();
     }
 
     public boolean doesHaveMane() {
         return hasMane;
     }
-    @Override
-    public List<String> eatMeat() throws Exception {
-        return getFood("Хищник");
-    }
 
-    public int getKittens() {
-        return getKittens(1);
-    }
-
-    public int getKittens(int kittensCount) {
-        return kittensCount;
+    public List<String> getFood() throws Exception {
+        return feline.getFood("Хищник");
     }
 }
